62a964aeb2fb0dbd18bc36c5c6eeeefc718323a8
yragae <yragae@icon-creations.com>
2019-02-17 12:15:05 +0200

    converted modules to static objects as per dagger guidelines

--- core/src/main/java/com/excuta/schwiftypedia/core/component/modules/networkclient/NetworkClientModule.kt
+++ core/src/main/java/com/excuta/schwiftypedia/core/component/modules/networkclient/NetworkClientModule.kt
@@ -23,6 +23,7 @@ object NetworkClientModule {
         return httpLoggingInterceptor
     }
 
+    @JvmStatic
     @CoreScope
     @Provides
     fun okHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
